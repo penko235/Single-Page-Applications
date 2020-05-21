69fc69d368cee5cb548a67b3a1368042ff867690
Penko Gadzhev <61637018+penko235@users.noreply.github.com>
2020-05-21 10:43:35 +0300

    Create firebase.js

---
+++ SoftTerest/config/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDbU5f_mDWD8o2Gji6xnEf34VrcJxQMPIg",
+    authDomain: "soft-terest.firebaseapp.com",
+    databaseURL: "https://soft-terest.firebaseio.com",
+    projectId: "soft-terest",
+    storageBucket: "soft-terest.appspot.com",
+    messagingSenderId: "29431378345",
+    appId: "1:29431378345:web:616c18ba727cd5e76ed4b8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
